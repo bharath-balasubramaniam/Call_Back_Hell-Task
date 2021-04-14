ee7a55a1cc6b315a82a01be013ec803a11cadc52
Bharath Balasubramaniam <79153733+bharath-balasubramaniam@users.noreply.github.com>
2021-04-14 23:23:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() =>{
+    let num = document.getElementById('no').innerHTML = 10;
+    setTimeout(() =>{
+        let num = document.getElementById('no').innerHTML = 9;
+        setTimeout(() =>{
+            let num = document.getElementById('no').innerHTML = 8;
+            setTimeout(() =>{
+                let num = document.getElementById('no').innerHTML = 7;
+                setTimeout(() =>{
+                    let num = document.getElementById('no').innerHTML = 6;
+                    setTimeout(() =>{
+                        let num = document.getElementById('no').innerHTML = 5;
+                        setTimeout(() =>{
+                            let num = document.getElementById('no').innerHTML = 4;
+                            setTimeout(() =>{
+                                let num = document.getElementById('no').innerHTML = 3;
+                                setTimeout(() =>{
+                                    let num = document.getElementById('no').innerHTML = 2;
+                                    setTimeout(() =>{
+                                        let num = document.getElementById('no').innerHTML = 1;
+                                        setTimeout(()=>{
+                                            let num = document.getElementById('no').innerHTML = 'Happy Independence Day';
+                                            num = document.getElementById('no').style.color = 'orange';
+                                        },1000)       
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
